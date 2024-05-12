339f1016a264c6ad70b05030079076e53a116547
SS Developer <77143468+SanuShaw@users.noreply.github.com>
2024-05-12 06:27:49 +0000

    badge updated

--- food/src/Components/Header.js
+++ food/src/Components/Header.js
@@ -23,9 +23,12 @@ const drawerWidth = 240;
 const StyledBadge = styled(Badge)(({ theme }) => ({
   "& .MuiBadge-badge": {
     right: -3,
-    top: 13,
+    // top: 13,
     border: `2px solid ${theme.palette.background.paper}`,
     padding: "0 4px",
+  },
+  "& >svg": {
+    color: "white"
   },
 }));
 
@@ -63,7 +66,7 @@ function Header(props) {
   return (
     <Box sx={{ display: "flex" }}>
       <CssBaseline />
-      <AppBar component="nav" sx={{ padding: 0 }}>
+      <AppBar component="nav" sx={{ backgroundColor:"black", padding: 0 }}>
         <Toolbar>
           <IconButton
             color="inherit"
